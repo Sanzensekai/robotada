1df0290a3c8f812f3f086c2a5c4f00fa71207338
Sanzensekai <beckler.nathael@gmail.com>
2013-12-02 11:19:55 +0100

    Spécification du site

--- sources/programmes/src/site.ads
+++ sources/programmes/src/site.ads
@@ -1,5 +1,18 @@
+with AdaGraph; use AdaGraph;
 
 package Site is
 
+type Place_Names is (I1, I2, ...)
+subtype Ring_Places is ...
+...
+function Way_Out(To: Output_Places) return Ring_Places;
+...
+protected Safely is
+procedure Draw_Site
+procedure Draw_Path
+procedure Draw_Robot
+procedure Hide_Robot
+... etc ...;
+end;
 
 end Site;
