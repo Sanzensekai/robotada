ec5b544ecf829f984265b2710c351e377d533fe2
Salwa El Rhazi <salwa.el-rhazi@uha.fr>
2013-12-02 17:40:07 +0100

    Implémentation de robot.adb et robot.ads

--- sources/programmes/src/robot.ads
+++ sources/programmes/src/robot.ads
@@ -1,11 +1,13 @@
 with Adagrah; use Adagraph;
-with AdaGraph.Color_Type; use AdaGraph.Color_Type;
+with AdaGraph.Color_Type;
+use AdaGraph.Color_Type;
 
 package Robot is
 
-   task type Object (Color: Color_Type) is private;
-   AdaGraph.Color_Type Color;
-   entry Follow(Road : Path);
-   entry ShutDown();
+   task type Object (Color: Color_Type) is
+
+   	entry Follow(Road : Path);
+      entry ShutDown();
+   end Object;
 
 end Robot;
